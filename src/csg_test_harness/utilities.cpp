eb4559027957f8d24c87fd1b7f9283156fefed68
Wahaj Syed <wahaj.nauman@gmail.com>
2021-05-26 16:22:16 +0000

    Add #defines, refactor into csg utilities, deserialize yaml

--- src/csg_test_harness/utilities.cpp
+++ src/csg_test_harness/utilities.cpp
@@ -1,2 +1,26 @@
-#ifdef CHIP_CSG_TEST_HARNESS
-#endif
+#include "constants.h"
+#include <string.h>
+std::string randomFromInitiator_str_key ("RandomFromInitiator");
+std::string PBKDFParamRequest_str_key ("PBKDFParamRequest");
+
+const char * yaml_string_for_map(std::map<std::string, std::map<std::string, std::string>> *trace_map)
+{
+    std::string result_yaml_str = "";
+    for (auto& x: *trace_map) {
+        // for each message
+        result_yaml_str += x.first;
+        result_yaml_str += ":\n";
+        for (auto& y: x.second) {
+            // for each message item
+            result_yaml_str += "  "; // indent 2 spaces for nested hash
+            result_yaml_str += y.first;
+            result_yaml_str += ": ";
+            result_yaml_str += y.second;
+            result_yaml_str += "\n";
+        }
+    }
+
+    char * response = new char [result_yaml_str.length()+1];
+    strcpy(response, result_yaml_str.c_str());
+    return response;
+}
