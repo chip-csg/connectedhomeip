543494a4046ae5f6564241288024a1a791f5c47d
Wahaj Syed <wahaj.nauman@gmail.com>
2021-07-14 14:14:30 -0700

    Refactor test client build scripts

--- src/controller/python/test_client/scripts/activate_environment.sh
+++ src/controller/python/test_client/scripts/activate_environment.sh
@@ -1,3 +1,6 @@
-export CHIP_SDK_ROOT=../../../..
-export TEST_CLIENT_ROOT=./
-source $CHIP_SDK_ROOT/scripts/activate.sh
+
+if [[ -z "${CHIP_SDK_ROOT}" ]]; then
+    export CHIP_SDK_ROOT=../../../..
+    export TEST_CLIENT_ROOT=./
+    source $CHIP_SDK_ROOT/scripts/activate.sh
+fi
